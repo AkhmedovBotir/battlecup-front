a8e4c3d0273988d7a0f269a4e15b4c260c8ba1f5
Akhmedov Botir <134036244+AkhmedovBotir@users.noreply.github.com>
2025-01-08 04:37:15 +0500

    new

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['localhost'],
-  }
+  },
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Battle Cup',
@@ -10,20 +7,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={inter.className}>
-      <body 
-        className={`min-h-screen bg-background text-white antialiased ${inter.className}`}
-        style={{
-          backgroundImage: "url('/img/login_bg.png')",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          backgroundAttachment: "fixed"
-        }}
-      >
-        <div className={`min-h-screen bg-[#0B0D10CC] ${inter.className}`}>
-          {children}
-        </div>
+    <html lang="en">
+      <body>
+        {children}
       </body>
     </html>
   )
